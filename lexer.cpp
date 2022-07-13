c03618fab496013e971ad3bd94a116c996a2204f
Sees <vsevolod.egorov@vicuesoft.com>
2022-07-14 00:39:57 +0300

    add Lexer class + Tokenizator subclass + Buffer subclass
    * Lexer aqumulate Tokenizator class
    * Tokenizator aqumulate Buffer subclass
    * Architecture is partly composition and partly my own chaos architecture
    * Now without inheritage, it's in plan
    * i think in the end, architecture should be one composititon class, many abstrack classes with inheritage

---
+++ lexer.cpp
@@ -0,0 +1,160 @@
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <fstream>
+
+enum Token = {
+  END = 1<<0, TEXT = 1<<1, OPENING_BLOCK_TAG_NAME = 1<<2,
+  CLOSING_BLOCK_TAG_NAME = 1<<3, EMPTY_TAG_NAME = 1<<4,
+  COMMENT = 1<<5, MACRO_TAG = 1<<6, ATTRIBUTE_NAME = 1<<7,
+  UNQUOTED_ATTRIBUTE_VALUE = 1<<7, SINGLE_QUOTED_ATTRIBUTE_VALUE = 1<<8,
+  DOUBLE_QUOTED_ATTRIBUTE_VALUE = 1<<9
+}
+
+class Buffer
+{
+public:
+
+  Buffer():bufCounter(0),isEnd(false)
+  {
+    buffer = new char [32];
+  }
+
+  ~Buffer()
+  {
+    delete [] buffer;
+  }
+
+  void getChar(char& c)
+  {
+    if (c == ' ' || c == '?' || c == '-') 
+    {
+      isEnd = true;
+      return;
+    else {
+      *(buffer + bufCounter) = c;
+      bufCounter++;
+    }
+  }
+
+  bool isFully()
+  {
+    return isEnd;
+  }
+
+private:
+  bool isEnd;
+  uint8_t bufCounter;
+  char* buffer;
+};
+
+class Tokenizator
+{
+public:
+
+  void init()
+  {
+    current_token_type = static_cast<int>(TEXT);
+  }
+
+
+
+  std::vector<Token> Tokenization (char& c)
+  {
+    switch (current_token_type)
+    {
+    case TEXT:
+    {
+      if (c == '>')
+      {
+        error_message = "unexpected ending!";
+        throw std::runtime_error(error_message);
+      }else if (c == '<'){
+        current_token_type = OPENING_BLOCK_TAG_NAME | CLOSING_BLOCK_TAG_NAME | EMPTY_TAG_NAME | COMMENT | MACRO_TEG;
+      }else{
+        buffer.getChar(c)
+        if(buffer.isFully()){
+
+        }
+        current_token_type = TEXT;
+      }
+
+     
+
+      break;
+    }
+    case OPENING_BLOCK_TAG_NAME | CLOSING_BLOCK_TAG_NAME | EMPTY_TAG_NAME | COMMENT | MACRO_TEG:
+    {
+      if (c == ' ') current_token_type = OPENING_BLOCK_TAG_NAME;
+      if (c == '/') current_token_type = CLOSING_BLOCK_TAG_NAME;
+      if (c == '!') current_token_type = EMPTY_TAG_NAME | COMMENT;
+      if (c == '?') current_token_type = MACRO_TEG;
+    }
+    case EMPTY_TAG_NAME | COMMENT:
+    {
+      if (c == '-') current_token_type = COMMENT;
+      else current_token_type = EMPTY_TAG_NAME;
+    }
+    case COMMENT:
+    {
+      if (c == '-') 
+      {
+        current_token_type = TEXT;
+        buffer.add(COMMENT);
+      }
+    }
+    }
+  }
+
+private:
+  Buffer buffer;
+  std::string error_message;
+  size_t current_token_type;
+  std::string buffer;
+};
+
+class Lexer
+{ 
+public:
+
+  Tokenizator MakeToken;
+  bool ready_to_tokenization = false;
+  unsigned long line;
+  unsigned long pos;
+  char c;
+
+public:
+
+  void init()
+  {
+    MakeToken.init();
+    line = 1;
+    pos = 1;
+    ready_to_tokenization = true;
+  }
+
+  void Disassemble(ifstream& file)
+  {
+    if(ready_to_tokenization == false) { std::cout << "make initalization of lexer first!";return;}
+
+    try
+    {
+      while ((c = file.get()) != EOF)
+        {
+          if (c != '\n')
+          {
+            pos++;
+            MakeToken(c);
+          }else{
+            pos = 1;
+            line++;
+          }
+        }
+    }catch(std::exception& e){
+
+    }
+
+  }
+
+};
+
--- main.cpp
+++ main.cpp
@@ -2,26 +2,25 @@
 #include <fstream>
 #include <iomanip>
 
-std::string ReadAll(const std::string &filename)
+std::string ReadAll (const std::string &filename)
 {
-    std::ifstream ifs;
-    ifs.open(filename);
-    ifs.seekg(0, std::ios::end);
-    size_t lenght = ifs.tellg();
-    ifs.seekg(0,std::ios::beg);
-    std::string buff (lenght, 0);
-    ifs.read(&buff[0], lenght);
-    ifs.close();
+  std::ifstream ifs;
+  ifs.open(filename);
+  ifs.seekg(0, std::ios::end);
+  size_t lenght = ifs.tellg();
+  ifs.seekg(0,std::ios::beg);
+  std::string buff (lenght, 0);
+  ifs.read(&buff[0], lenght);
+  ifs.close();
 
-    return buff;
+  return buff;
 }
 
-void WriteAll(std::string &row_seq)
+void WriteAll (std::string &row_seq)
 {
-    
+   std::cout << row_seq;
 }
 
 int main(){
-    std::string row_seq = ReadAll("sait.html");
-    std::cout<<"First word is = "<<word;
+
 }
